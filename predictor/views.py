23ed6854f5397d2177c99aedb4cf711a04f6b184
scorp_jr <biruk.scorpion.dereje@gmail.com>
2026-01-02 20:40:58 +0300

    DecisionTree using Iris Dataset

---
+++ predictor/views.py
@@ -0,0 +1,27 @@
+import joblib
+import numpy as np
+from django.shortcuts import render
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+model_path = os.path.join(BASE_DIR, "decision_tree_pipeline.joblib")
+
+model = joblib.load(model_path)
+
+def home(request):
+    prediction = None
+
+    if request.method == "POST":
+        X = np.array([[
+            float(request.POST["sepal_length"]),
+            float(request.POST["sepal_width"]),
+            float(request.POST["petal_length"]),
+            float(request.POST["petal_width"]),
+        ]])
+        
+        labels = ["Setosa", "Versicolor", "Virginica"]
+        prediction = labels[model.predict(X)[0]]
+
+    return render(request, "index.html", {
+        "prediction": prediction
+    })
